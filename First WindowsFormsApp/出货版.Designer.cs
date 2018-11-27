24e27f54bfe52b6449d115425e179c5d8d42fc4d
anny chen <948750083@qq.com>
2018-11-27 11:05:36 +0800

    添加项目文件。

---
+++ First WindowsFormsApp/出货版.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace First_WindowsFormsApp
+{
+    partial class 出货版
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(出货版));
+            this.label1 = new System.Windows.Forms.Label();
+            this.KtextBox = new System.Windows.Forms.TextBox();
+            this.btnxy = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numtextBox = new System.Windows.Forms.TextBox();
+            this.btnnumber = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tBopenfile = new System.Windows.Forms.TextBox();
+            this.tBgpxs = new System.Windows.Forms.TextBox();
+            this.userCurve1 = new HslCommunication.Controls.UserCurve();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.butpic = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "系数K";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // KtextBox
+            // 
+            this.KtextBox.Location = new System.Drawing.Point(87, 415);
+            this.KtextBox.Name = "KtextBox";
+            this.KtextBox.Size = new System.Drawing.Size(106, 21);
+            this.KtextBox.TabIndex = 1;
+            this.KtextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnxy
+            // 
+            this.btnxy.Location = new System.Drawing.Point(502, 413);
+            this.btnxy.Name = "btnxy";
+            this.btnxy.Size = new System.Drawing.Size(75, 23);
+            this.btnxy.TabIndex = 2;
+            this.btnxy.Text = "取出KB系数";
+            this.btnxy.UseVisualStyleBackColor = true;
+            this.btnxy.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "系数B";
+            // 
+            // BtextBox
+            // 
+            this.BtextBox.Location = new System.Drawing.Point(304, 415);
+            this.BtextBox.Name = "BtextBox";
+            this.BtextBox.Size = new System.Drawing.Size(100, 21);
+            this.BtextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "仪器序列号";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // numtextBox
+            // 
+            this.numtextBox.Location = new System.Drawing.Point(116, 9);
+            this.numtextBox.Name = "numtextBox";
+            this.numtextBox.Size = new System.Drawing.Size(100, 21);
+            this.numtextBox.TabIndex = 6;
+            this.numtextBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnnumber
+            // 
+            this.btnnumber.Location = new System.Drawing.Point(42, 469);
+            this.btnnumber.Name = "btnnumber";
+            this.btnnumber.Size = new System.Drawing.Size(174, 54);
+            this.btnnumber.TabIndex = 7;
+            this.btnnumber.Text = "调试版修改序列号";
+            this.btnnumber.UseVisualStyleBackColor = true;
+            this.btnnumber.Click += new System.EventHandler(this.btnnumber_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 22);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "出货版修改";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(565, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 34);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "绘制gpxs曲线图";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "打开gpxs文件";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // tBopenfile
+            // 
+            this.tBopenfile.Location = new System.Drawing.Point(116, 41);
+            this.tBopenfile.Name = "tBopenfile";
+            this.tBopenfile.Size = new System.Drawing.Size(387, 21);
+            this.tBopenfile.TabIndex = 13;
+            // 
+            // tBgpxs
+            // 
+            this.tBgpxs.Location = new System.Drawing.Point(35, 72);
+            this.tBgpxs.Multiline = true;
+            this.tBgpxs.Name = "tBgpxs";
+            this.tBgpxs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tBgpxs.Size = new System.Drawing.Size(263, 322);
+            this.tBgpxs.TabIndex = 14;
+            // 
+            // userCurve1
+            // 
+            this.userCurve1.AllowDrop = true;
+            this.userCurve1.BackColor = System.Drawing.Color.Transparent;
+            this.userCurve1.IsAbscissaStrech = true;
+            this.userCurve1.Location = new System.Drawing.Point(315, 91);
+            this.userCurve1.Name = "userCurve1";
+            this.userCurve1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.userCurve1.Size = new System.Drawing.Size(666, 303);
+            this.userCurve1.StrechDataCountMax = 402;
+            this.userCurve1.TabIndex = 15;
+            this.userCurve1.ValueMaxLeft = 3F;
+            this.userCurve1.ValueMaxRight = 3F;
+            this.userCurve1.ValueSegment = 10;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(374, 484);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(138, 24);
+            this.btnsave.TabIndex = 16;
+            this.btnsave.Text = "保存当前页面值";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // butpic
+            // 
+            this.butpic.Location = new System.Drawing.Point(374, 514);
+            this.butpic.Name = "butpic";
+            this.butpic.Size = new System.Drawing.Size(138, 24);
+            this.butpic.TabIndex = 17;
+            this.butpic.Text = "截图保存";
+            this.butpic.UseVisualStyleBackColor = true;
+            this.butpic.Click += new System.EventHandler(this.butpic_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(733, 474);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 49);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "触发版修改";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // 出货版
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 550);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.butpic);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.userCurve1);
+            this.Controls.Add(this.tBgpxs);
+            this.Controls.Add(this.tBopenfile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnnumber);
+            this.Controls.Add(this.numtextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnxy);
+            this.Controls.Add(this.KtextBox);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "出货版";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox KtextBox;
+        private System.Windows.Forms.Button btnxy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BtextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox numtextBox;
+        private System.Windows.Forms.Button btnnumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tBopenfile;
+        private System.Windows.Forms.TextBox tBgpxs;
+        private HslCommunication.Controls.UserCurve userCurve1;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button butpic;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ First WindowsFormsApp/出货版.cs
@@ -0,0 +1,419 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace First_WindowsFormsApp
+{
+    public partial class 出货版 : Form
+    {
+
+        string gpxs = null;
+        string CD = System.IO.Directory.GetCurrentDirectory();
+
+        public object ValueMaxLeft { get; private set; }
+
+        public 出货版()
+        {
+            InitializeComponent();
+        }
+
+        //第一行修改产品的序列号
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnnumber_Click(object sender, EventArgs e)
+        {
+            /*可以替换序列号了，同一个文件里若多次修改会出现中文乱码，所以采用向另一个文件输出的方式*/
+            /*调试版仅修改序列号*/
+            string num = numtextBox.Text;
+            string path = CD+"\\lcd1.c";
+            string con = "";
+            // string oldnum = "\d\d";
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(path, Encoding.GetEncoding("gb2312"));
+            con = sr.ReadToEnd();
+            //con = con.Replace("S/N:HN18Z0", "S/N:HN18Z0"+num);
+
+            Regex regex = new Regex(@"S/N:HN18Z0\d\d");
+            con = regex.Replace(con, "S/N:HN18Z0" + num);
+            sr.Close();
+            fs.Close();
+            FileStream fs2 = new FileStream("F:\\在线光谱仪\\20180718\\小糸车灯光谱版本 - 调试版 -beat 1.1\\USER\\lcd.c", FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs2);
+            sw.WriteLine(con);
+            sw.Close();
+            fs2.Close();
+            MessageBox.Show("序列号修改成功！");
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //替换KB系数，标识行：pixel=4.0141*(380+j)-1002.4; //**号
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //提取K,B系数 "F:\\我的winform\\bcdbxishu.txt"
+            StreamReader sr3 = new StreamReader(@"C:\\Users\Administrator\\Desktop\\20180828调试软件\\bcdbxishu.txt", Encoding.GetEncoding("gb2312"));
+                    string K = sr3.ReadLine();
+                    string[] sline1 = K.Trim().Split('\r');
+                    KtextBox.Text = sline1[0];
+                    string B = sr3.ReadLine();
+                    string[] sline2 = B.Trim().Split('\r');
+                    BtextBox.Text = sline2[0];
+
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+          
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            ////修改序列号
+            //numhandler(CD + "\\lcd1.c", "F:\\在线光谱仪\\20180718\\小糸车灯光谱版本 - 出货版 -beat 1.1 -614\\小糸车灯光谱版本 - 出货版 -beat 1.1 - 副本\\USER\\lcd.c");
+
+
+            //这里修改gpxs数据格式
+            gpxshandler();
+            //这里把修改gpxs数据格式后的文件保存到一个数组
+            StreamReader sr4 = new StreamReader(CD+@"\\gpxs1.txt", Encoding.GetEncoding("gb2312"));
+            string strLine;
+    
+            while (sr4.EndOfStream == false)
+            {
+                try
+                {
+                    strLine = sr4.ReadToEnd();
+                    gpxs = Convert.ToString(strLine);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            sr4.Dispose();
+
+
+           
+
+            //这里修改.c文件里的gpxs KB,   标志位：//HereAddNewGpxsOfMachine
+            string num = numtextBox.Text;
+            string path1 = CD+@"\\出货版collect1.c";
+            string path2 = @"F:\\在线光谱仪\\20180718\小糸车灯光谱版本 - 出货版 -beat 1.1 -614\\小糸车灯光谱版本 - 出货版 -beat 1.1 - 副本\\USER\\collect.c";
+            //string path2 = @"F:\\我的winform\\20180718\\小糸车灯光谱版本 - 出货版 -beat 1.1 -614\\小糸车灯光谱版本 - 出货版 -beat 1.1 - 副本\\USER\\collect.c";
+            string con = "";
+
+            //清空collect文件
+            FileStream stream2 = File.Open(path2, FileMode.OpenOrCreate, FileAccess.Write);
+            stream2.Seek(0, SeekOrigin.Begin);
+            stream2.SetLength(0);
+            stream2.Close();
+
+
+            FileStream fs5 = new FileStream(path1, FileMode.Open, FileAccess.Read);
+            StreamReader sr5 = new StreamReader(path1, Encoding.GetEncoding("gb2312"));
+            con = sr5.ReadToEnd();
+           
+            //修改gpxs
+            con = con.Replace("HereAddNewGpxsOfMachine", num+ "号的gpxs\r\n" + gpxs+ "\r\n//HereAddNewGpxsOfMachine");
+            //替换K,B系数
+            string numK = KtextBox.Text;
+            string numB = BtextBox.Text;
+            Regex regex1 = new Regex(@"pixel\=4\.0141\*\(380\+j\)\-1002\.4");
+            con = regex1.Replace(con, "pixel=" + numK + "*(380+j)" + numB);
+            MessageBox.Show("KB修改成功！");
+
+
+            sr5.Close();
+            fs5.Close();
+            FileStream fs6 = new FileStream(path2, FileMode.Open, FileAccess.Write);
+            StreamWriter sw5 = new StreamWriter(fs6);
+            sw5.Write(con);
+            sw5.Close();
+            fs6.Close();
+            MessageBox.Show("出货版修改成功！");
+
+        }
+
+        //这里修改gpxs数据格式 
+        private void gpxshandler()
+        {
+
+          
+            string num = numtextBox.Text;
+            string path1 = @"C:\\Users\Administrator\\Desktop\\20180828调试软件\\gpxs.txt";
+            //string path1 = @"F:\\我的winform\\gpxs.txt";
+            string path2 = CD+@"\\gpxs1.txt";
+            string con1 = "";
+
+            FileInfo fi = new FileInfo(path2);
+            if (!fi.Exists)//如果文件不存在
+            {
+                fi.Create();//创建文件
+            }
+
+
+            FileStream fs2 = new FileStream(path1, FileMode.Open, FileAccess.Read);
+            StreamReader sr1 = new StreamReader(fs2);
+            con1 = sr1.ReadToEnd();
+            con1 = con1.Replace("\r", ",");
+            con1 = con1.Remove(con1.LastIndexOf(","), 1);
+            sr1.Close();
+            fs2.Close();
+            FileStream fs3 = new FileStream(path2, FileMode.Open, FileAccess.Write);
+            StreamWriter sw1 = new StreamWriter(fs3);
+            sw1.WriteLine(con1);
+            sw1.Close();
+            fs3.Close();
+            MessageBox.Show("gpxs1.txt修改格式成功！");
+
+        }
+
+        //这里修改产品序列号  输入原始参考文件lcd1.c,输出到参与编译烧入的程序lcd.c中
+        private void numhandler(string path1, string path2)
+        {
+            string num = numtextBox.Text;
+            string con = "";
+            FileStream fs = new FileStream(path1, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(path1, Encoding.GetEncoding("gb2312"));
+            con = sr.ReadToEnd();
+
+            Regex regex = new Regex(@"S/N:HN18Z0\d\d");
+            con = regex.Replace(con, "S/N:HN18Z0" + num);
+            sr.Close();
+            fs.Close();
+            FileStream fs2 = new FileStream(path2, FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs2);
+            sw.WriteLine(con);
+            sw.Close();
+            fs2.Close();
+            MessageBox.Show("序列号修改成功！");
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //图表的文件打开
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open File";
+            ofd.Filter = "txt文件|*.txt|所有文件|*.*";
+            double[] temp = new double[1000];
+            int Len = 0;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tBopenfile.Text = ofd.FileName;
+                FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+
+                string strLine;
+
+                while (sr.EndOfStream == false)
+                {
+                    try
+                    {
+                        strLine = sr.ReadLine();
+                        tBgpxs.Text = tBgpxs.Text + strLine + "\r\n";
+                        temp[Len] = Convert.ToDouble(strLine);
+                        Len++;
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                sr.Dispose();
+                fs.Dispose();
+            }
+
+            for (int i = 0; i <= Len; i++)
+            {
+                if (temp[i] < 0)
+                {
+                    MessageBox.Show("gpxs有负数！请联系研发人员！");
+                    break;
+                }
+                else   //绘制曲线图
+                {
+                   
+                    float[] temp2= Array.ConvertAll(temp, d => (float)d);
+                    userCurve1.ValueMaxLeft = temp2.Max();
+                    userCurve1.ValueMaxRight = temp2.Max();
+                    userCurve1.SetLeftCurve("A", temp2, Color.DodgerBlue);
+                }
+            }
+            //显示数据到曲线图表
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            string num = numtextBox.Text;
+            string word1 = "\r\n\\\\" + num + "号的gpxs\r\n";
+            string word2 = "\r\n\\\\" + num + "号的K,B";
+            string word3 = "\r\n\\\\" + DateTime.Now.ToLocalTime().ToString(); 
+            string numKB = "\r\n" + KtextBox.Text + "\r\n" + BtextBox.Text+ "\r\n";
+            string pathjl = CD + "\\gpxs记录.txt";
+
+           FileInfo fi = new FileInfo(pathjl);
+            if (!fi.Exists)//如果文件不存在
+            {
+                fi.Create();//创建文件
+            }
+
+            FileStream fs2 = File.OpenWrite(pathjl);//打开文件
+            fs2.Seek(0, SeekOrigin.End);//指向文件头
+            byte[] b4 = Encoding.Default.GetBytes(word3.ToString());//将录入的内容转换为byte数组
+            fs2.Write(b4, 0, b4.Length);//写入内容 当前日期
+            byte[] b1 = Encoding.Default.GetBytes(word1.ToString());//将录入的内容转换为byte数组
+            fs2.Write(b1, 0, b1.Length);//写入内容序列号
+            byte[] b2 = Encoding.Default.GetBytes(gpxs.ToString());//将录入的内容转换为byte数组
+            fs2.Write(b2, 0, b2.Length);//写入内容gpxs
+            byte[] b3 = Encoding.Default.GetBytes(word2.ToString());//将录入的内容转换为byte数组
+            fs2.Write(b3, 0, b3.Length);//写入内容kb
+            byte[] numkb = Encoding.Default.GetBytes(numKB.ToString());//将录入的内容转换为byte数组
+            fs2.Write(numkb, 0, numkb.Length);//写入内容
+            fs2.Close();//关闭文件
+            MessageBox.Show("操作成功");
+
+        }
+
+        private void butpic_Click(object sender, EventArgs e)
+        {
+            Bitmap bit = new Bitmap(this.Width, this.Height);//实例化一个和窗体一样大的bitmap
+            Graphics g = Graphics.FromImage(bit);
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;//质量设为最高
+            g.CopyFromScreen(this.Left, this.Top, 0, 0, new Size(this.Width, this.Height));//保存整个窗体为图片
+                                                                                           //g.CopyFromScreen(panel游戏区 .PointToScreen(Point.Empty), Point.Empty, panel游戏区.Size);//只保存某个控件（这里是panel游戏区）
+
+            string subPath = CD + "/pic/";
+            if (false == System.IO.Directory.Exists(subPath))
+            {
+                //创建pic文件夹
+                System.IO.Directory.CreateDirectory(subPath);
+            }
+            
+            string num = numtextBox.Text;
+            string filename = num+"号的截图.png";
+            string path2 = subPath + filename;
+            bit.Save(path2);//默认保存格式为PNG，保存成jpg格式质量不是很好
+            MessageBox.Show("截图保存成功！");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //修改产品序列号
+            numhandler(CD + "\\lcd1.c", "F:\\在线光谱仪\\20180718\\小糸车灯光谱版本 - 触发版 -beat 1.1 -0705 - 副本\\小糸车灯光谱版本 - 出货版 -beat 1.1 - 副本\\USER\\lcd.c");
+
+            //gpxs1文件打开  保存到string：gpxs
+            MessageBox.Show("请先将这台仪器的gpxs粘贴进gpxs1.txt文件内！");
+            MessageBox.Show("请先将这台仪器的K,B系数粘K,B方框内！");
+            MessageBox.Show("请选择gpxs1.txt文件！");
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Open File";
+            ofd.Filter = "txt文件|*.txt|所有文件|*.*";
+            double[] temp = new double[1000];
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tBopenfile.Text = ofd.FileName;
+                FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+
+                string strLine;
+
+                while (sr.EndOfStream == false)
+                {
+                    try
+                    {
+                        strLine = sr.ReadToEnd();
+                        gpxs = Convert.ToString(strLine);
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+                sr.Dispose();
+                fs.Dispose();
+
+                ////这里修改.c文件里的gpxs KB,和序列号   标志位：//HereAddNewGpxsOfMachine
+                string num = numtextBox.Text;
+                string path1 = CD+@"\\触发版collect1.c";
+                string path2 = @"F:\\在线光谱仪\\20180718\\小糸车灯光谱版本 - 触发版 -beat 1.1 -0705 - 副本\\小糸车灯光谱版本 - 出货版 -beat 1.1 - 副本\\USER\\collect.c";
+                //string path2 = @"F:\\我的winform\\20180718\\小糸车灯光谱版本 - 触发版 -beat 1.1 -0705 - 副本\\小糸车灯光谱版本 - 出货版 -beat 1.1 - 副本\\USER\\collect.c";
+                string con = "";
+
+                //清空collect文件
+                FileStream stream2 = File.Open(path2, FileMode.OpenOrCreate, FileAccess.Write);
+                stream2.Seek(0, SeekOrigin.Begin);
+                stream2.SetLength(0);
+                stream2.Close();
+
+                FileStream fs5 = new FileStream(path1, FileMode.Open, FileAccess.Read);
+                StreamReader sr5 = new StreamReader(path1, Encoding.GetEncoding("gb2312"));
+                con = sr5.ReadToEnd();
+
+                //修改gpxs
+                con = con.Replace("HereAddNewGpxsOfMachine", num + "号的gpxs\r\n" + gpxs + "\r\n//HereAddNewGpxsOfMachine");
+                //替换K,B系数
+                string numK = KtextBox.Text;
+                string numB = BtextBox.Text;
+                Regex regex1 = new Regex(@"pixel\=4\.0141\*\(380\+j\)\-1002\.4");
+                con = regex1.Replace(con, "pixel=" + numK + "*(380+j)" + numB);
+                MessageBox.Show("KB修改成功！");
+
+
+                sr5.Close();
+                fs5.Close();
+                FileStream fs6 = new FileStream(path2, FileMode.Open, FileAccess.Write);
+                StreamWriter sw5 = new StreamWriter(fs6);
+                sw5.Write(con);
+                sw5.Close();
+                fs6.Close();
+                MessageBox.Show("出货版修改成功！");
+
+            }
+        }
+    }
+
+
+
+}
